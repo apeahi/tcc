148670c23c3fb58a0d940c51e2581353fab01f5d
apeaahi <apaeahi.tcc@gmail.com>
2018-05-09 16:06:44 -1000

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction(a) {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        } else {
+            x.className = "topnav";}
+    a.classList.toggle("change");
+} 
+
+
+// COMPTE A REBOURS
+function timerfunction(){
+
+    console.log('Test')
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 30, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Time is up ";
+  }
+}, 1000);
+
+}
+
